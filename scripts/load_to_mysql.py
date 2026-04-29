4b49ad7f37f015ebabe9f5975ff140829cd07c78
SAMYSBL <samara.barcelos@live.com>
2026-04-29 15:58:06 -0300

    conexão sql e python, inserção de dados nas tabelas, criação do .env

---
+++ scripts/load_to_mysql.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+import pandas as pd
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+products_path = BASE_DIR / 'processed' / 'products.csv'
+sizes_path = BASE_DIR / 'processed' / 'sizes.csv'
+
+products_df = pd.read_csv(products_path)
+sizes_df = pd.read_csv(sizes_path)
+
+load_dotenv()
+
+conn = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
+)
+
+cursor = conn.cursor()
+
+#inserir produtos
+for _, row in products_df.iterrows():
+    cursor.execute("""
+        INSERT INTO products (name, price, product_type, category)
+        VALUES (%s, %s, %s, %s)
+    """, (row['name'],
+          float['price'], 
+          row['product_type'],
+          row['category'],
+          row['brand']
+          ))
+    #inserir sizes
+    for _, row in sizes_df.iterrows():
+        cursor.execute("""
+        INSERT INTO sizes (product_id, size)
+        VALUES (%s, %s)
+    """, (
+        int(row['product_id']),
+        row['size']
+    ))
+    
+    #salvando
+    conn.commit()
+
+print("Dados inseridos!")
+
+cursor.close()
+conn.close()
